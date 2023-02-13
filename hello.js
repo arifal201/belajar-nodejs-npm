f4e5c5e50d7ff4889ddc8fc146d4364137e70a66
arifal201 <arifalhidayat10@gmail.com>
2023-02-13 20:37:24 +0700

    belajar menggunakan library yang buatan sendiri

---
+++ hello.js
@@ -0,0 +1,10 @@
+import { SayHello, sum } from "belajar-membuat-npm-library-arifal";
+import {min,max} from "belajar-membuat-npm-library-arifal/number";
+
+console.info(SayHello("arifal hidayat"));
+
+console.info(sum([10,10,10,10,10,10]));
+
+console.info(min(10,20));
+
+console.info(max(10,20));
